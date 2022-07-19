a95ff7496a8361d68487020ae62899d57d3a0c2a
anjupaulvathiyath <anjupaulvathiyath@hotmail.com>
2022-07-19 10:16:40 +0400

    first commit

---
+++ src/test/java/qaclickacademy/MavenJava/APITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.MavenJava;
+
+import org.junit.Test;
+
+public class APITest {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/MavenJava/AppiumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.MavenJava;
+
+import org.junit.Test;
+
+public class AppiumTest {
+
+
+	@Test
+	public void nativeAppAndroid()
+	{
+		System.out.println("nativeAppAndroid");
+	}
+	
+	@Test
+	public void iOSApp()
+	{
+		System.out.println("iOSApp");
+	}
+
+}
